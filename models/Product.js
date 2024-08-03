38b9651d4fde67c8008d16a67a2f14102dc43b32
Douglas Santos <dgstos94@gmail.com>
2024-08-03 12:34:34 -0300

    feat: create Product model

---
+++ models/Product.js
@@ -0,0 +1,8 @@
+class Product {
+    constructor(title, price, image, description) {
+        this.title = title;
+        this.price = price;
+        this.image = image;
+        this.description = description;
+    }
+}
