c687c05eb273d5bacf4695b4d912259fd6c81025
Douglas Santos <dgstos94@gmail.com>
2024-08-03 21:41:58 -0300

    ref: create helper to extract page info extraction logical

--- controllers/product.js
+++ controllers/product.js
@@ -1,82 +1,17 @@
-const express = require('express');
-const puppeteer = require("puppeteer");
 const fs = require('fs');
 const path = require('path');
+const extractProductInfo = require('../helpers/extractProductInfo');
 const os = require('os');
 
-const Product = require('../models/Product');
 
 module.exports = {
     getProductInfo: async ({ body: { productUrl } }, res) => {
-        const browser = await puppeteer.launch({ headless: "false" });
-        
-        const page = await browser.newPage();
+        const product = await extractProductInfo(productUrl);
 
-        await page.goto(
-          productUrl, {waitUntil: 'networkidle2'}
-        );
-        
-        const content = await page.evaluate(() => {
-          return document.querySelector(".showcase");
-        });
-            
-        const description = await page.evaluate(() => {
-          return content.querySelector(".features--description").textContent.trim();
-        });
-      
-        const price = await page.evaluate(() => {
-          return content.querySelector(".saleInCents-value").textContent.trim();
-        });
-      
-        const title = await page.evaluate(() => {
-          return content.querySelector(".product-name").textContent.trim();
-        });
-      
-        const image = await page.evaluate(() => {
-          return content.querySelector('img.float-buybox-wrap-product__image').src;
-        });
-        
-        const product =  new Product(
-            title,
-            price,
-            image,
-            description
-        );
-        
         res.status(200).send(product);
-        await page.close();
     },
-    getProductInfoCsv: async () => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-
-      await page.goto("https://www.netshoes.com.br/p/creatina-max-titanium-monohidratada-natural-A05-4154-001");
-      const content = await page.evaluate(() => {
-        return document.querySelector(".showcase");
-      });
-          
-      const description = await page.evaluate(() => {
-        return content.querySelector(".features--description").textContent.trim();
-      });
-    
-      const price = await page.evaluate(() => {
-        return content.querySelector(".saleInCents-value").textContent.trim();
-      });
-    
-      const title = await page.evaluate(() => {
-        return content.querySelector(".product-name").textContent.trim();
-      });
-    
-      const image = await page.evaluate(() => {
-        return content.querySelector('img.float-buybox-wrap-product__image').src;
-      });
-      
-      const product =  new Product(
-          title,
-          price,
-          image,
-          description
-      );
+    getProductInfoCsv: async ({ body: { productUrl } }, res) => {
+      const product = await extractProductInfo("https://www.netshoes.com.br/p/creatina-max-titanium-monohidratada-natural-A05-4154-001");
 
       const csvContent = [
         ['Name', 'Price', 'Description', 'Image'],
@@ -94,7 +29,7 @@ module.exports = {
           console.log('csv salvo: ', filePath);
 
       })
-
       await browser.close();
+      res;
     }
 }
---
+++ helpers/extractProductInfo.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+
+const Product = require('../models/Product');
+
+module.exports = async function extractProductInfo (productUrl) {
+        const browser = await puppeteer.launch({ headless: "false" });
+        
+        const page = await browser.newPage();
+
+        await page.goto(
+          productUrl, {waitUntil: 'networkidle2'}
+        );
+        
+        const content = await page.evaluate(() => {
+          return document.querySelector(".showcase");
+        });
+            
+        const description = await page.evaluate(() => {
+          return content.querySelector(".features--description").textContent.trim();
+        });
+      
+        const price = await page.evaluate(() => {
+          return content.querySelector(".saleInCents-value").textContent.trim();
+        });
+      
+        const title = await page.evaluate(() => {
+          return content.querySelector(".product-name").textContent.trim();
+        });
+      
+        const image = await page.evaluate(() => {
+          return content.querySelector('img.float-buybox-wrap-product__image').src;
+        });
+        
+        const product =  new Product(
+            title,
+            price,
+            image,
+            description
+        );
+        
+        await page.close();
+
+        return product;
+    }
